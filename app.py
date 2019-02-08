d1b9f497775b5c0adcd30fb38a683d1a718675dc
dyfontaine <dylan.fontaine12@gmail.com>
2019-02-08 02:09:32 -0600

    added boto3 to requirements

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash_table_experiments import DataTable
-import plotly.graph_objs as go
  
 import pandas as pd
 from itertools import compress
